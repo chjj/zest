868aa791b54606bf58fe1cab7e9194c4502ce0f7
Christopher Jeffrey <chjjeffrey@gmail.com>
2011-08-12 19:26:23 -0500

    fix test

--- test/index.js
+++ test/index.js
@@ -2,6 +2,6 @@ var connect = require('connect')
   , app = connect.createServer();
 
 app.use(connect.static(__dirname));
-app.use(connect.static(__dirname + '/..'));
+app.use(connect.static(__dirname + '/../lib'));
 
 app.listen(8080);
