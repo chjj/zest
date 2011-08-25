2554088960a879ccd96c3e84c827dea587b278b8
Christopher Jeffrey <chjjeffrey@gmail.com>
2011-08-25 17:49:18 -0500

    test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,25 @@
 var connect = require('connect')
   , app = connect.createServer();
 
+var static = connect.static;
+connect.static = function() {
+  var func = static.apply(connect, arguments);
+  return function(req, res, next) {
+    var setHeader = res.setHeader;
+    res.setHeader = function(name) {
+      res.setHeader = setHeader;
+      switch (name) {
+        case 'Cache-Control':
+        case 'Last-Modified':
+        case 'ETag':
+          return;
+      }
+      return setHeader.apply(res, arguments);
+    };
+    return func(req, res, next);
+  };
+};
+
 app.use(connect.static(__dirname));
 app.use(connect.static(__dirname + '/../lib'));
 
