9dce32bf42174976e719930b2dd8e093c52deca9
Christopher Jeffrey <chjjeffrey@gmail.com>
2011-08-25 18:05:53 -0500

    use express for testing

--- test/index.js
+++ test/index.js
@@ -1,26 +1,21 @@
-var connect = require('connect')
-  , app = connect.createServer();
+var express = require('express')
+  , app = express.createServer();
 
-var static = connect.static;
-connect.static = function() {
-  var func = static.apply(connect, arguments);
-  return function(req, res, next) {
-    var setHeader = res.setHeader;
-    res.setHeader = function(name) {
-      res.setHeader = setHeader;
-      switch (name) {
-        case 'Cache-Control':
-        case 'Last-Modified':
-        case 'ETag':
-          return;
-      }
-      return setHeader.apply(res, arguments);
-    };
-    return func(req, res, next);
+app.use(function(req, res, next) {
+  var setHeader = res.setHeader;
+  res.setHeader = function(name) {
+    switch (name) {
+      case 'Cache-Control':
+      case 'Last-Modified':
+      case 'ETag':
+        return;
+    }
+    return setHeader.apply(res, arguments);
   };
-};
+  next();
+});
 
-app.use(connect.static(__dirname));
-app.use(connect.static(__dirname + '/../lib'));
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/../lib'));
 
 app.listen(8080);
