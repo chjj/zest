78ee407e3607d18e9f14b4df106936ed7ff5f569
Christopher Jeffrey <chjjeffrey@gmail.com>
2012-02-27 11:21:57 -0600

    cleanup

--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+var spawn = require('child_process').spawn;
+
 var express = require('express')
   , app = express.createServer();
 
@@ -17,5 +19,13 @@ app.use(function(req, res, next) {
 
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/../lib'));
+
+var proto = express.static(__dirname + '/proto');
+app.use('/proto', function(req, res, next) {
+  var child = spawn(__dirname + '/proto/update');
+  child.on('exit', function() {
+    proto(req, res, next);
+  });
+});
 
 app.listen(8080);
